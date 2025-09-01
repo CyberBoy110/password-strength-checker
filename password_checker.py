8b3a7b8444f83bca696fa528864265a1cdc8b61a
Jack <132793224+CyberBoy110@users.noreply.github.com>
2025-09-01 14:40:03 -0400

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,1 +1,36 @@
 
+
+import re
+
+def check_password_strength(password):
+  # Minimum Length
+  score = 0
+  if len(password) >= 8:
+    score += 1
+    
+  # Uppercase letters
+  if re.search(r"[A-Z]", password):
+    score += 1
+  
+  # Lowercase letters
+  if re.search(r"[a-z]", password):
+    score += 1
+  
+  
+  # Numbers
+  if re.search(r"[0-9]", password):
+    score += 1
+  
+  # Special Characters
+  if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+    score += 1
+  
+  
+  
+  #return score
+  if score <= 2:
+      return "Weak"
+  elif score == 3 or score == 4:
+    return "Medium"
+  else:
+    return  "Strong"
